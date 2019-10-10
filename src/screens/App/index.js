73ba103444e01c861309267db1232c5a5cd099b7
Humano Laranja <contato.carlos@outlook.com>
2019-10-10 10:10:22 -0300

    added images, styles and show members list

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../../styles/main.css';
 
 export default class App extends Component {
   state = {
     currentInputText: '',
+    currentHighlight: '',
     lastTeamChoosed: null,
     total: 24,
     teams: [
       {
         name: 'Bulbassauro',
+        color: '#374D2C',
+        imageUrl: 'bulbassauro.png',
         members: []
       },
       {
         name: 'Charmander',
+        color: '#8F4A43',
+        imageUrl: 'charmander.png',
         members: []
       },
       {
         name: 'Squirtle',
+        color: '#334555',
+        imageUrl: 'squirtle.png',
         members: []
       },
       {
         name: 'Pikachu',
+        color: '#E4BE43',
+        imageUrl: 'pikachu.png',
         members: []
       },
     ]
+  }
+
+  handleGetAllNames = () => {
+    const { teams } = this.state;
+    const names = teams.map((el) => el.name);
+
+    return names;
   }
 
   handleCalculateMaxTeamMembers = () => {
@@ -38,7 +55,7 @@ export default class App extends Component {
     const sizes = teams.map((el) => el.members.length);
     const maxlength = this.handleCalculateMaxTeamMembers();
 
-    if(sizes[teamIndex] >= maxlength) {
+    if (sizes[teamIndex] >= maxlength) {
       return true;
     }
     return false;
@@ -49,7 +66,7 @@ export default class App extends Component {
     const sizes = teams.map((el) => el.members.length);
     const alreadyFull = sizes.reduce((a, b) => a + b)
 
-    if(alreadyFull >= total) {
+    if (alreadyFull >= total) {
       return true;
     }
     return false;
@@ -71,6 +88,7 @@ export default class App extends Component {
 
   handleDrawTeam = () => {
     const { lastTeamChoosed } = this.state;
+    // TODO try to make this state machine generic
     const states = {
       0: [1, 2, 3],
       1: [0, 2, 3],
@@ -79,7 +97,9 @@ export default class App extends Component {
     };
     let teamIndex = null;
 
-    if (lastTeamChoosed === null) {
+    if (this.handleAlreadyFull()) {
+      teamIndex = null;
+    } else if (lastTeamChoosed === null) {
       teamIndex = Math.floor(Math.random() * Object.keys(states).length);
     }
     else if (this.handleGetCurrentMaxTeam(false) - this.handleGetCurrentMinTeam(false) > 1) {
@@ -87,10 +107,6 @@ export default class App extends Component {
     }
     else {
       do {
-        if(this.handleAlreadyFull()) {
-          teamIndex = null;
-          break;
-        }
         teamIndex = states[lastTeamChoosed][Math.floor(Math.random() * (Object.keys(states).length - 1))];
       } while (teamIndex === this.handleGetCurrentMaxTeam() || this.handleTeamAlreadyFull(teamIndex));
     }
@@ -106,26 +122,54 @@ export default class App extends Component {
     const { currentInputText, teams } = this.state;
     const drawnTeamIndex = this.handleDrawTeam();
     const drawnTeam = teams[drawnTeamIndex];
-    if(drawnTeam) {
+    if (drawnTeam) {
       const newTeam = [{ ...drawnTeam, members: [...drawnTeam.members, currentInputText] }]
       const newTeams = teams.map(obj => newTeam.find(o => o.name === obj.name) || obj);
-  
+
       this.setState({ currentInputText: '', teams: newTeams, lastTeamChoosed: drawnTeamIndex });
     } else {
-      alert('Acabou');
+      alert('Total number of people reached');
     }
     event.preventDefault();
   }
 
+  componentDidMount() {
+    let total;
+    do {
+      total = prompt('How many people in total?', 24);
+    } while (!total || isNaN(total.replace(/\s/g, "-")));
+
+    this.setState({ total });
+  }
+
   render() {
-    const { currentInputText } = this.state;
+    const { currentInputText, currentHighlight, teams } = this.state;
 
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={currentInputText} onChange={this.handleChange} />
-          <input type="submit" value="Eu escolho você" />
-        </form>
+        <div className="form-container">
+          <form className="form-container" onSubmit={this.handleSubmit}>
+            <input type="text" value={currentInputText} onChange={this.handleChange} />
+            <input type="submit" value=">" />
+          </form>
+        </div>
+        <div className="teams-container">
+          {teams.map((team, index) => {
+            return (
+              <div key={`${index}-${team.name}`} className={'team-container ' + (team.name === currentHighlight ? 'highlight' : '')} style={{ backgroundColor: team.color }}>
+                <h1>{team.name}</h1>
+                <img src={require(`../../assets/images/${team.imageUrl}`)} alt={team.name} />
+                <div className="members-container">
+                  {team.members.map((member, index) => {
+                    return (
+                      <h4 key={`${index}-${team.name}`}>{member}</h4>
+                    )
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     )
   }
